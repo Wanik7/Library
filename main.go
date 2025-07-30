9edaae81042ca96e716ed23d559fc22b6e63a0fe
Wanik7 <negroespopa@gmail.com>
2025-07-30 20:38:49 +0300

    Release

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"library/utils"
+	"log"
+	"strings"
+)
+
+func main() {
+	// if you want to add a book
+	err := utils.AddBook("Alexander Pushkin", "", 1823, []string{"something"})
+	if err != nil {
+		log.Fatalf("Error: %+v", err)
+	}
+
+	// if you want to see all books in the library
+	books, err := utils.LoadBooks()
+	if err != nil {
+		log.Fatalf("Error: %+v", err)
+	}
+	for _, book := range books {
+		var genres string
+		for _, genre := range book.Genres {
+			genres = genre + ", "
+		}
+		genres = strings.TrimRight(genres, ", ")
+		fmt.Printf("Title: %s; Author: %s; Release year: %d; Genres: %s\n", book.Title, book.Author, book.Year, genres)
+	}
+
+	// if you want to find certain author books
+	tolkiensBooks, err := utils.FindBooksByAuthor("Tolkien")
+	if err != nil {
+		log.Fatalf("Error: %+v", err)
+	}
+	for _, book := range tolkiensBooks {
+		var genres string
+		for _, genre := range book.Genres {
+			genres = genre + ", "
+		}
+		genres = strings.TrimRight(genres, ", ")
+		fmt.Printf("Title: %s; Author: %s; Release year: %d; Genres: %s\n", book.Title, book.Author, book.Year, genres)
+	}
+
+	// if you want to delete book by its title
+	err = utils.DeleteBookByTitle("The Hobbit")
+	if err != nil {
+		log.Fatalf("Error: %+v", err)
+	}
+}
---
+++ utils/utils.go
@@ -0,0 +1,110 @@
+package utils
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+type Book struct {
+	Title  string   `json:"title"`
+	Author string   `json:"author"`
+	Year   int      `json:"year"`
+	Genres []string `json:"genres"`
+}
+
+func LoadBooks() ([]Book, error) { // i use this function like sub-function in other functions
+	data, err := os.ReadFile("utils/library.json")
+	if err != nil {
+		return []Book{}, errors.New("read file")
+	}
+	var booksVault []Book
+	err = json.Unmarshal(data, &booksVault)
+	if err != nil {
+		return []Book{}, errors.New("parse from json")
+	}
+
+	return booksVault, nil
+}
+
+func AddBook(title string, author string, year int, genres []string) error {
+	switch {
+	case title == "":
+		return errors.New("book title cannot be empty")
+	case author == "":
+		return errors.New("book author cannot be empty")
+	case year <= 0 || year > time.Now().Year():
+		return errors.New("invalid year")
+	case len(genres) == 0:
+		return errors.New("book must contains at least one genre")
+	default:
+		newBook := Book{
+			Title:  title,
+			Author: author,
+			Year:   year,
+			Genres: genres,
+		}
+
+		books, err := LoadBooks()
+		if err != nil {
+			return errors.New(err.Error())
+		}
+
+		for _, v := range books { // check for duplicates
+			if v.Author == newBook.Author && v.Title == newBook.Title && v.Year == newBook.Year {
+				return errors.New("this book already exists in library")
+			}
+		}
+
+		books = append(books, newBook)
+		toJson, _ := json.MarshalIndent(books, "", "  ")
+		os.WriteFile("utils/library.json", toJson, 0644)
+		
+		fmt.Println("The book was append")
+		return nil
+	}
+}
+
+func FindBooksByAuthor(author string) ([]Book, error) {
+	books, err := LoadBooks()
+		if err != nil {
+			return []Book{}, errors.New(err.Error())
+		}
+
+	var booksByAuthor []Book
+	for _, v := range books {
+		if strings.Contains(v.Author, author) {
+			booksByAuthor = append(booksByAuthor, v)
+		}
+	}
+	if len(booksByAuthor) == 0 {
+		return []Book{}, errors.New("there is no books")
+	}
+	return booksByAuthor, nil
+}
+
+func DeleteBookByTitle(title string) error {
+	books, err := LoadBooks()
+	if err != nil {
+			return errors.New(err.Error())
+		}
+
+	var newBooks []Book
+	for _, v := range books {
+		if v.Title != title {
+			newBooks = append(newBooks, v)
+		}
+	}
+	if len(newBooks) == len(books) {
+		return errors.New("this book is not in library")
+	}
+
+	toJson, _ := json.MarshalIndent(newBooks, "", "  ")
+	os.WriteFile("utils/library.json", toJson, 0644)
+
+	fmt.Println("The book was removed")
+	return nil
+}
